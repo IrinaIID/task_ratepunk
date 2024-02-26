2b8c444c66d14d84cbd7f17e180bdad716776ec2
Irina Ischenko <28myhome@gmail.com>
2024-02-26 17:51:50 +0200

    feat: add mobile menu and copy link

--- ratepunk/src/app/blog/page.tsx
+++ ratepunk/src/app/blog/page.tsx
@@ -1,8 +1,9 @@
 import Header from "../components/header/heder";
+import styles from './page.module.scss';
 
 export default function Blog() {
   return (
-    <main>
+    <main className={styles.mainBlog}>
       <Header />
       <p>BLOg</p>
     </main>
--- ratepunk/src/app/components/form/form.tsx
+++ ratepunk/src/app/components/form/form.tsx
@@ -1,36 +1,90 @@
 'use client';
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import styles from './form.module.scss';
+import SuccessSvg from '@/assets/svg/success';
 
 export default function Form() {
 
-  const [valueInput, setValueInput] = useState<string | null>(null)
-  // const [error, setError] = useState<string | null>(null)
+  const LINK_REFERRAL = `https://ratepunk.com/referral`;
+
+  const [valueInput, setValueInput] = useState<string | null>(null);
+  const [error, setError] = useState<boolean | null>(false);
+  const [isLink, setIsLink] = useState<boolean>(false);
+
+
+  function onChangeText(e: ChangeEvent<HTMLInputElement>) {
+    const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{1,})$/iu;
+    setValueInput(e.currentTarget.value)
+    let isEmail = false;
+    if (valueInput) {
+      isEmail = EMAIL_REGEXP.test(valueInput)
+    }
+    setError(!isEmail)
+  }
  
-  // async function onSubmit(event: { preventDefault: () => void; currentTarget: HTMLFormElement | undefined; }) {
-  //   event.preventDefault()
-  //   setError(null) // Clear previous errors when a new request starts
-  //   const formData = new FormData(event.currentTarget)
-  //   console.log(formData)
-  // }
+  function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    if (!error && valueInput) {
+      setIsLink(true)
+    }
+  }
+
+  function onSubmitLink(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+  }
+
+
+
+
+
+
 
   return (
-    <div>
-      {/* {error && <div style={{ color: 'red' }}>{error}</div>} */}
-      <form className={styles.allForm}>
-        <input 
-          type='email'
-          className={styles.inputForm}
-          placeholder='Enter your email address'
-          onChange={e => { setValueInput(e.currentTarget.value) }} 
-        />
-        <button 
-          type='submit'
-          className={styles.buttonForm}
+    <>
+      {!isLink && <div>
+        {error && 
+          <div className={styles.messageErrorEmail}>Error State</div> 
+        }
+        <form 
+          className={styles.allForm}
+          onSubmit={e => onSubmit(e)}
         >
-          Get Referral Link
-        </button>
-      </form>
-    </div>
+          <input 
+            type='email'
+            className={styles.inputForm}
+            placeholder='Enter your email address'
+            onChange={e => onChangeText(e)} 
+          />
+          <button  type='submit'className={styles.buttonForm}>
+            Get Referral Link
+          </button>
+        </form>
+      </div>}
+      {isLink &&
+        <div className={styles.blockLink}>
+          <div className={styles.descriptionLinkBlock}>
+            <SuccessSvg />
+            <p className={styles.descriptionLinkText}>Your email is confirmed!</p>
+          </div>
+          <form 
+            className={styles.allFormLink}
+            onSubmit={e => onSubmitLink(e)}
+          >
+            <input 
+              type='text'
+              className={styles.inputFormLink}
+              value={LINK_REFERRAL}
+            />
+            <button  
+              type='submit'className={styles.buttonFormLink}
+              onClick={() =>  navigator.clipboard.writeText(`${LINK_REFERRAL}`)}
+            >
+              Copy
+            </button>
+          </form>
+
+        </div>
+      }
+    </>
   );
 }
